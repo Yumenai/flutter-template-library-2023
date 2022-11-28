7bbb3f8db934ab81e6f73309d6a10bd130f09d9b
WeiJianQuek <weijianquek@gmail.com>
2022-11-28 10:34:13 +0800

    Add notification service to support app notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import UIKit
+import Firebase
 import Flutter
+import UIKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,25 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
+
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
+
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
+    } else {
+      let settings: UIUserNotificationSettings = UIUserNotificationSettings(
+        types: [.alert, .badge, .sound],
+        categories: nil
+      )
+      application.registerUserNotificationSettings(settings)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/service/notification_service.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static Future<void> setup() async {
+    await Firebase.initializeApp();
+
+    /// When user open the app through the notification
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+
+    });
+
+    /// Update the iOS foreground notification presentation options to allow heads up notifications.
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.instance.subscribeToTopic('all');
+
+    if (Platform.isAndroid) {
+      FirebaseMessaging.onMessage.listen((remoteMessage) {
+        final notification = remoteMessage.notification;
+        final android = remoteMessage.notification?.android;
+
+        if (notification != null && android != null) {
+          const channel = AndroidNotificationChannel(
+            'high_importance_channel', // id
+            'High Importance Notifications', // title
+            importance: Importance.high,
+          );
+
+          FlutterLocalNotificationsPlugin().initialize(
+            const InitializationSettings(
+              android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+            ),
+            onDidReceiveNotificationResponse: (response) {
+              /// When notification is interacted
+            },
+            onDidReceiveBackgroundNotificationResponse: (response) {
+              /// When notification is interacted
+            },
+          );
+
+          FlutterLocalNotificationsPlugin().show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                icon: '@mipmap/ic_launcher', // Use black and white icon
+                color: Colors.transparent,
+              ),
+            ),
+          );
+        }
+      });
+    }
+  }
+
+  const NotificationService();
+
+  Future<String> getToken() async {
+    return await FirebaseMessaging.instance.getToken() ?? '';
+  }
+}
