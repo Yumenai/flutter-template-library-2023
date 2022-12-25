b6d61b88ab74edc029651d78ff0ea3815d377787
WeiJianQuek <weijianquek@gmail.com>
2022-12-25 17:43:08 +0800

    Add menu button component to handle nested menu option

---
+++ lib/component/button/menu_button_component.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class MenuButtonComponent extends StatelessWidget {
+  final _buttonKey = GlobalKey<PopupMenuButtonState>();
+
+  final List<MenuButtonItem> itemList;
+
+  MenuButtonComponent({
+    Key? key,
+    required this.itemList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MenuButtonItem? model;
+
+    return PopupMenuButton<int>(
+      key: _buttonKey,
+      itemBuilder: (context) {
+        if (model == null) {
+          return [
+            for (int i = 0; i < itemList.length; i++)
+              PopupMenuItem(
+                value: i,
+                child: Text(itemList[i].title ?? ''),
+              ),
+          ];
+        } else {
+          return [
+            for (int i = 0; i < (model?.itemList.length ?? 0); i++)
+              PopupMenuItem(
+                value: i,
+                child: Text(model?.itemList[i].title ?? ''),
+              ),
+          ];
+        }
+      },
+      onSelected: (position) {
+        if (model == null) {
+          model = itemList[position];
+        } else {
+          model = model?.itemList[position];
+        }
+
+        if (model?.itemList.isEmpty ?? true) {
+          model?.onSelect();
+          model = null;
+        } else {
+          _buttonKey.currentState?.showButtonMenu();
+        }
+      },
+      onCanceled: () {
+        model = null;
+      },
+    );
+  }
+}
+
+class MenuButtonItem {
+  final String? title;
+  final void Function() onSelect;
+  final List<MenuButtonItem> itemList;
+
+  const MenuButtonItem({
+    required this.title,
+    this.itemList = const [],
+    required this.onSelect,
+  });
+}
