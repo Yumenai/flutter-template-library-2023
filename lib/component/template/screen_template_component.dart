c4b4d61b1f7b3abd4e5110300c21e9b8ab0109cc
WeiJianQuek <weijianquek@gmail.com>
2023-01-25 10:32:23 +0800

    Add collapsible style in screen template component

--- lib/component/template/screen_template_component.dart
+++ lib/component/template/screen_template_component.dart
@@ -1,5 +1,48 @@
 import 'package:flutter/material.dart';
 
+Widget? _infoComponent({
+  required final Widget? infoIconPrefix,
+  required final Widget? infoIconSuffix,
+  required final String? infoTitle,
+  final Color? foregroundColor,
+}) {
+  final titleWidgetList = <Widget> [];
+
+  if (infoIconPrefix is Widget) {
+    titleWidgetList.add(infoIconPrefix);
+    titleWidgetList.add(const SizedBox(
+      width: 8,
+    ));
+  }
+
+  if (infoTitle?.isNotEmpty == true) {
+    titleWidgetList.add(Text(
+      infoTitle ?? '',
+      style: TextStyle(
+        color: foregroundColor,
+      ),
+    ));
+  }
+
+  if (infoIconSuffix is Widget) {
+    titleWidgetList.add(const SizedBox(
+      width: 8,
+    ));
+    titleWidgetList.add(infoIconSuffix);
+  }
+
+  if (titleWidgetList.isEmpty) {
+    return null;
+  } else if (titleWidgetList.length > 1) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: titleWidgetList,
+    );
+  } else {
+    return titleWidgetList.first;
+  }
+}
+
 class ScreenTemplateComponent extends StatelessWidget {
   final String? infoTitle;
   final Widget? infoIconPrefix;
@@ -37,7 +80,11 @@ class ScreenTemplateComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _headerLayout(),
+        title: _infoComponent(
+          infoIconPrefix: infoIconPrefix,
+          infoIconSuffix: infoIconSuffix,
+          infoTitle: infoTitle,
+        ),
         actions: infoActionList,
         backgroundColor: enableOverlapHeader ? Colors.transparent : null,
         elevation: enableOverlapHeader ? 0 : null,
@@ -58,37 +105,89 @@ class ScreenTemplateComponent extends StatelessWidget {
       bottomNavigationBar: navigatorBottom,
     );
   }
+}
 
-  Widget? _headerLayout() {
-    final titleWidgetList = <Widget> [];
+class CollapsibleScreenTemplateComponent extends StatelessWidget {
+  final String? infoTitle;
+  final Widget? infoIconPrefix;
+  final Widget? infoIconSuffix;
+  final List<Widget>? infoActionList;
+  final Widget? infoBackground;
 
-    if (infoIconPrefix is Widget) {
-      titleWidgetList.add(infoIconPrefix ?? const SizedBox());
-      titleWidgetList.add(const SizedBox(
-        width: 8,
-      ));
-    }
+  final Widget? layout;
+  final Widget? layoutAction;
 
-    if (infoTitle?.isNotEmpty == true) {
-      titleWidgetList.add(Text(infoTitle ?? ''));
-    }
+  final Widget? navigatorLeft;
+  final Widget? navigatorRight;
+  final Widget? navigatorBottom;
 
-    if (infoIconSuffix is Widget) {
-      titleWidgetList.add(const SizedBox(
-        width: 8,
-      ));
-      titleWidgetList.add(infoIconSuffix ?? const SizedBox());
-    }
+  final Color? foregroundColor;
+  final Color? backgroundColor;
 
-    if (titleWidgetList.isEmpty) {
-      return null;
-    } else if (titleWidgetList.length > 1) {
-      return Row(
-        mainAxisSize: MainAxisSize.min,
-        children: titleWidgetList,
-      );
-    } else {
-      return titleWidgetList.first;
-    }
+  const CollapsibleScreenTemplateComponent({
+    Key? key,
+    this.infoTitle,
+    this.infoIconPrefix,
+    this.infoIconSuffix,
+    this.infoActionList,
+    this.infoBackground,
+    this.layout,
+    this.layoutAction,
+    this.navigatorLeft,
+    this.navigatorRight,
+    this.navigatorBottom,
+    this.foregroundColor,
+    this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final topSpacing = MediaQuery.of(context).padding.top + kToolbarHeight - 16;
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, isInnerBoxScrolled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: false,
+              actions: infoActionList,
+              expandedHeight: MediaQuery.of(context).size.width,
+              foregroundColor: foregroundColor,
+              flexibleSpace: FlexibleSpaceBar(
+                title: LayoutBuilder(
+                  builder: (context, constraint) {
+                    final opacity = topSpacing / constraint.constrainHeight();
+
+                    return Opacity(
+                      opacity: opacity < 0.5 ? 0 : opacity,
+                      child: _infoComponent(
+                        infoIconPrefix: infoIconPrefix,
+                        infoIconSuffix: infoIconSuffix,
+                        infoTitle: infoTitle,
+                        foregroundColor: Theme.of(context).colorScheme.onSurface,
+                      ),
+                    );
+                  },
+                ),
+                centerTitle: true,
+                background: infoBackground,
+              ),
+            ),
+          ];
+        },
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: layout,
+        ),
+      ),
+      floatingActionButton: layoutAction,
+      drawer: navigatorLeft,
+      endDrawer: navigatorRight,
+      bottomNavigationBar: navigatorBottom,
+    );
   }
 }
